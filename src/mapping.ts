09e48c0554bbc8560ecb5357a186cddc7b3501e5
@codingsh <codingsh@pm.me>
2021-03-08 22:15:18 -0300

    feat(mapping): add REB2 events query ref:https://github.com/Ditto-money/ProjectManagement/issues/22

--- src/mapping.ts
+++ src/mapping.ts
@@ -2,11 +2,25 @@ import { BigInt } from "@graphprotocol/graph-ts"
 import {
   SwapDeposit as SwapDepositEvent
 } from "../generated/DittoTokenSwap/DittoTokenSwap"
-import { SwapDeposit } from "../generated/schema"
+import { SwapDeposit, SwapDepositREB2 } from "../generated/schema"
 
 
 export function handleSwapDeposit(event: SwapDepositEvent): void {
   let swapDitto = new SwapDeposit(
+    event.transaction.hash.toHex() + "-" + event.logIndex.toString()
+  );
+
+  swapDitto.depositor = event.params.depositor;
+  swapDitto.input = event.params.input;
+  swapDitto.inputAmount = event.params.inputAmount;
+  swapDitto.outputAmount = event.params.outputAmount;
+
+  swapDitto.save();
+}
+
+
+export function handleSwapDepositREB2(event: SwapDepositEvent): void {
+  let swapDitto = new SwapDepositREB2(
     event.transaction.hash.toHex() + "-" + event.logIndex.toString()
   );
 
